9492a55df2007deb97affbe3566c013672eba51e
Moritz Heidkamp <moritz.heidkamp@bevuta.com>
2010-11-16 16:22:02 +0100

    help

--- server/static/app.js
+++ server/static/app.js
@@ -1,11 +1,20 @@
 $(function() {
+  var help = $("<div/>").attr("id", "help")
+    .append($("<div/>").attr("class", "content").html("\
+  <h2>Help</h2> \
+  \
+  <dl> \
+    <dt>foo</dt> \
+    <dd>bar</dd> \
+  </dl> \
+ "));
 
   function div(id) {
-    return $("<div/>").attr({ id: id }).appendTo($("body"))
+    return $("<div/>").attr({ id: id }).appendTo($("body"));
   }
 
-  function button(label) {
-    return $("<button/>").attr({ type: "button" }).text(label);
+  function button(label, id) {
+    return $("<button/>").attr({ type: "button", id: id }).text(label);
   }
 
   function refresh() {
@@ -61,6 +70,24 @@ $(function() {
     setTimeout(function() { message.fadeOut("fast", function() { message.remove() }); }, 1000);
   }
 
+  var showHelp = (function() {
+    var originalWidth = null
+    
+    return function() {
+      var textarea = $("#tasks textarea");
+
+      if (help.is(":visible")) {
+        textarea.width(originalWidth);
+        help.hide();
+      } else {
+        originalWidth = textarea.width();
+        textarea.width("68%");
+        help.width("30%");
+        help.show();
+      }
+    }
+  })();
+
   function showControls() {
     div("controls")
       .append($("<label/>").attr({ "for": "user" }).text("User: "))
@@ -71,7 +98,8 @@ $(function() {
       .append($("<input/>").attr({ type: "text", id: "filter", name: "filter", value: $.query.get("filter") }).keyup(refresh))
       .append($("<input/>").attr({ type: "checkbox", id: "include-done", name: "include-done", value: $.query.get("include-done") }).keyup(refresh))
       .append($("<label/>").attr({ "for": "include-done" }).text(" include done tasks "))
-      .append(button("Show").click(refresh));
+      .append(button("Show", "show-tasks").click(refresh))
+      .append(button("?", "show-help").click(showHelp));
 
     $("#user").get(0).focus();
     refresh();
@@ -100,8 +128,7 @@ $(function() {
 	request.setRequestHeader("Accept", "text/x-downtime");
       },
       success: function(data) {
-	$("#tasks").data("user", user).html($("<textarea/>").attr({ cols: "80", rows: "30" }).val(data))
-	  .append(button("Save").click(save));
+	$("#tasks").data("user", user).find("textarea").val(data); 
       },
       error: handleRequestError
     });
@@ -110,7 +137,8 @@ $(function() {
   div("messages");
   $("body").append($("<h1/>").text("pox"));
   showControls();
-  div("tasks");
+  div("tasks").append($("<textarea/>")).append(help);
+  
   $(document).keyup(function(event) {
     if (event.ctrlKey && event.altKey && event.keyCode == 83) {
       save();
