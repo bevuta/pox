a29c50236373577371f8885e7cbe43347af24bd6
Moritz Heidkamp <moritz.heidkamp@bevuta.com>
2011-08-25 14:12:07 +0200

    update help

--- server/static/app.js
+++ server/static/app.js
@@ -1,18 +1,59 @@
 $(function() {
   var help = $("<div/>").attr("id", "help")
-    .append($("<div/>").attr("class", "content").html("\
-  <h2>Help</h2> \
-  <h3>Task Line Syntax</h3> \
-  <pre>* [description] (#([id]:[revision])? [meta-data])?</pre> \
-  Examples: \
-  <dl> \
-    <dt>A new task for yourself:</dt> \
-    <dd><pre>* do something</pre></dd> \
-    <dt>An existing task with the ID 274, revision 2:</dt> \
-    <dd><pre>* make stuff #274:2</pre></dd> \
-  </dl> \
-  \
- "));
+        .append($("<div/>").attr("class", "content").html(
+[
+"",
+"<h2>Help</h2>",
+"<h3>Syntax</h3>",
+"<pre>task     = &quot;*&quot; &lt;name&gt; &lt;meta&gt;? (&quot;\\n&quot; &lt;description&gt;)? ",
+"name     = [^#\\n]+",
+"meta     = &quot;#&quot; (&lt;id&gt; (&quot;:&quot; &lt;revision&gt;)?)? &lt;property&gt;*",
+"id       = [:digit:]+",
+"revision = [:digit:]+",
+"property = &quot;&gt;&quot; &lt;assignee&gt; | &quot;&lt;&quot; &lt;assigner&gt; | ",
+"           [+-] &lt;priority&gt; | &quot;:&quot; &lt;tag&gt;",
+"grouping = &quot;#&quot;+ &lt;property&gt;*</pre>",
+"<h3>Task Examples</h3>",
+"<dl>",
+"<dt>A new task for yourself:</dt>",
+"<dd>",
+"<pre>* do something</pre></dd>",
+"<dt>An existing task with the ID 274, revision 2:</dt>",
+"<dd>",
+"<pre>* make stuff #274:2</pre></dd>",
+"<dt>Delegate task to user frank:</dt>",
+"<dd>",
+"<pre>* fix it # &gt; frank</pre></dd>",
+"<dt>A task with priority 9 assigned to you by joe:</dt>",
+"<dd>",
+"<pre>* add frob feature # +9 &lt; joe</pre></dd></dl>",
+"<h3>Grouping</h3>",
+"<p>Tasks can be grouped by preceding them with a &lt;grouping&gt;",
+"line. Groupings can be nested, nesting levels are indicated by the",
+"amount of leading &quot;#&quot; characters. Groupings cascade their properties",
+"to all tasks below their level.</p>",
+"<h3>Grouping Examples</h3>",
+"<dl>",
+"<dt>Two tasks for frank:</dt>",
+"<dd>",
+"<pre># &gt; frank",
+"* do this # +2",
+"* do that</pre></dd>",
+"<dt>Priorities are assigned from the nearest grouping:</dt>",
+"<dd>",
+"<pre># +9 :bug",
+"* this is priority +9 and tagged 'bug'",
+"## -5",
+"* this is priority -5 and also tagged 'bug'",
+"* this is priority +1 and also tagged 'bug' # +1</pre></dd>",
+"<dt>Tags are cascaded additively:</dt>",
+"<dd>",
+"<pre># :bug",
+"* this is an important bug # :important",
+"## :critical",
+"* this is a critical bug</pre></dd></dl>",
+].join("\n")
+        ));
 
   function div(id) {
     return $("<div/>").attr({ id: id }).appendTo($("body"));
