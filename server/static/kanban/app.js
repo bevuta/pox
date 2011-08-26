a89351332a00a3a8feb8028a34f588dfd219ecbe
Moritz Heidkamp <moritz.heidkamp@bevuta.com>
2011-08-26 17:19:25 +0200

    beginning of kanban querying

--- server/static/kanban/app.js
+++ server/static/kanban/app.js
@@ -4,26 +4,67 @@ var tag = function(tag, attrs) {
     return el;
 };
 
+var input = function(id, label) {
+    return tag("input", { id: id })
+        .before(tag("label", { for: id }).text(label));
+};
+
+var loadTasks = function() {
+    var baseuri = "/users/" +
+        encodeURIComponent($("#user").val()) +
+        "/tasks?filter=" +
+        encodeURIComponent($("#gfilter").val());
+    console.log(encodeURIComponent($("#gfilter").val()));
+
+    $(".column").each(function() {
+        var col = $(this);
+        var filter = col.find(".filter").val();
+        var uri = baseuri;
+
+        if (filter.length) {
+            uri += encodeURIComponent(" " + filter);
+        }
+
+        $.ajax({
+            type: "GET",
+            dataType: "json",
+            url: uri,
+            success: function(tasks) {
+                console.log(tasks);
+                col.find(".tasks")
+                    .text(tasks);
+            }
+        });
+    });
+};
+
 $(function() {
     var controls = tag("div", { id: "controls" });
     var columns  = tag("tr");
-    var user     = tag("input", { id: "user" })
-        .appendTo(controls)
-        .before(tag("label", { for: "user" }).text("User"));
-    var load   = tag("button").text("Load").appendTo(controls);
-    
-    $("body")
-        .append(controls)
-        .append(tag("table", { id: "columns" })
-                .append(columns));
+    var user     = input("user", "User").appendTo(controls);
+    var gfilter  = input("gfilter", "Filter").appendTo(controls);
 
     tag("button")
         .text("Add column")
         .click(function() {
-            var col = tag("td")
-                .text("a column")
-                .addClass("column");
-            columns.append(col);
+            var col = tag("td", { class: "column" }).appendTo(columns);
+
+            tag("input", { class: "filter" })
+                .appendTo(col)
+                .focus(); 
+
+            tag("div", { class: "tasks" }).
+                appendTo(col);
         })
         .appendTo(controls);
+
+    tag("button")
+        .text("Load")
+        .appendTo(controls)
+        .click(loadTasks);
+
+    $("body")
+        .append(controls)
+        .append(tag("table", { id: "columns" })
+                .append(columns));
 });
