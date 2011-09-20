717cf2b2108ddc598b0cc55915c7dd2d7a5b63a1
Moritz Heidkamp <moritz@twoticketsplease.de>
2011-09-20 12:00:35 +0200

    add updater and updater_id to tasks_with_tags view

--- sql/views.scm
+++ sql/views.scm
@@ -1,15 +1,15 @@
-(load-relative "../pox-db")
-(load-relative "../pox-db/helpers")
+(load-relative "../pox.system")
+(load-system pox)
 
 (use postgresql ssql ssql-postgresql)
 (use pox-db pox-db/helpers)
 
 (define task-query-columns
-  '(id created_at revision name description priority done category assignee_id assigner_id creator_id))
+  '(id created_at revision name description priority done category assignee_id assigner_id creator_id updater_id))
 
 (define task-tags-sub-query
   '(as (coalesce (select (call array_agg tag)
-                   (from (as (select (columns tag) 
+                   (from (as (select (columns tag)
                                (from (as (call unnest (call array_agg tag)) tag)))
                              tags))
                    (where (not (null? tag))))
@@ -17,29 +17,33 @@
        tags))
 
 (define full-tasks-query
-  (let ((columns `(assignee assigner creator . ,task-query-columns)))
+  (let ((columns `(assignee assigner creator updater . ,task-query-columns)))
     `(select (columns tags . ,columns)
-       (from 
+       (from
         (as (select (columns ,task-tags-sub-query . ,columns)
-              (from 
+              (from
                (as (select (columns (as u1.name assignee)
                                     (as u2.name assigner)
                                     (as u3.name creator)
+                                    (as u4.name updater)
                                     (as tags.name tag)
                                     . ,(map (lambda (c) `(col t ,c))
                                             task-query-columns))
                      (from (join left
-                                 (join left 
+                                 (join left
                                        (join left
                                              (join left
-                                                   (join left 
-                                                         (as tasks t) 
-                                                         (as users u1) 
-                                                         (on (= t.assignee_id u1.id))) 
-                                                   (as users u2) 
-                                                   (on (= t.assigner_id u2.id)))
-                                             (as users u3)
-                                             (on (= t.creator_id u3.id)))
+                                                   (join left
+                                                         (join left
+                                                               (as tasks t)
+                                                               (as users u1)
+                                                               (on (= t.assignee_id u1.id)))
+                                                         (as users u2)
+                                                         (on (= t.assigner_id u2.id)))
+                                                   (as users u3)
+                                                   (on (= t.creator_id u3.id)))
+                                             (as users u4)
+                                             (on (= t.updater_id u4.id)))
                                        (as task_tags tt)
                                        (on (= tt.task_id t.id)))
                                  tags
