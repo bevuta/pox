6cacc430e1a1d04ab24eb13e13cee2904467b61a
Moritz Heidkamp <moritz@twoticketsplease.de>
2011-09-20 15:42:48 +0200

    add default config for vandusen notification module

--- pox-notification/vandusen.scm
+++ pox-notification/vandusen.scm
@@ -1,14 +1,20 @@
 (module pox-notification/vandusen
 
-()
+(vandusen-config)
 
 (import chicken scheme)
 (use tcp data-structures extras pox-notification)
 
+(define vandusen-config
+  (make-parameter '()))
+
+(define (config-ref setting params)
+  (or (alist-ref setting params) (alist-ref setting (vandusen-config))))
+
 (register-notification-handler 'vandusen
   (lambda (user params changes)
     (receive (in out)
-        (tcp-connect (alist-ref 'host params) (alist-ref 'port params))
+        (tcp-connect (config-ref 'host params) (config-ref 'port params))
       (for-each (lambda (change)
                   (fprintf out "~A ~A~%"
                            (alist-ref 'recipient params)
