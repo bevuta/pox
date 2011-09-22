f79045289c2b5eef45ec6bb82df2a28fdb1e8b9b
Moritz Heidkamp <moritz@twoticketsplease.de>
2011-09-22 22:18:15 +0200

    log pox-mail/smtp send-mail calls

--- pox-mail/smtp.scm
+++ pox-mail/smtp.scm
@@ -3,11 +3,14 @@
 (smtp-config smtp-send)
 
 (import chicken scheme)
-(use hato-smtp)
+(use hato-smtp pox-log)
+
+(define-logger log mail smtp)
 
 (define smtp-config (make-parameter '()))
 
 (define (smtp-send . args)
+  (log (debug) (cons 'mail args))
   (apply send-mail (append (smtp-config) args)))
 
 )
