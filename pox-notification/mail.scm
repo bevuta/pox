1999a66f5a2441990f006e73a0fd4cb2173e98be
Moritz Heidkamp <moritz@twoticketsplease.de>
2011-09-28 22:57:35 +0200

    pox-notification/mail: add option to attach a unified diff of the changed tasks list to notification mails

--- pox-notification/mail.scm
+++ pox-notification/mail.scm
@@ -1,17 +1,42 @@
 (module pox-notification/mail
 
-(notification-mail-from)
+(notification-mail-from attach-diff-to-notification-mails?)
 
 (import chicken scheme)
-(use extras data-structures pox-notification pox-mail pox-model)
+(use extras ports posix data-structures srfi-1
+     pox-notification pox-mail pox-model downtime format-textdiff)
+
+(define attach-diff-to-notification-mails? (make-parameter #f))
+
+(define (diff-strings old new)
+  (let* ((strings (map (lambda (t)
+                         (string-split t "\n"))
+                       (list new old)))
+         (hunks (apply textdiff (append strings '(3)))))
+    (receive (in out pid)
+        (process "filterdiff -v --format=unified")
+      ((make-format-textdiff 'context) out hunks
+       "old" "" "new" "")
+      (close-output-port out)
+      (read-string #f in))))
+
+(define (changes->string changes user)
+  (let ((new-tasks (task-list->string (map second changes) user))
+        (old-tasks (task-list->string (filter identity (map third changes)) user)))
+    (values new-tasks (and (attach-diff-to-notification-mails?)
+                           (diff-strings new-tasks old-tasks)))))
 
 (define notification-mail-from (make-parameter #f))
 
 (register-notification-handler 'mail
   (lambda (user notifyee params changes)
-    (send-mail To:      (alist-ref 'recipient params)
-               From:    (or (alist-ref 'from params) (notification-mail-from))
-               Subject: (sprintf (or (alist-ref 'subject params) "[pox] changes by ~A") user)
-               Body:    (task-list->string (map cadr changes) notifyee))))
+    (receive (new-tasks diff)
+        (changes->string changes notifyee)
+      (send-mail To:      (alist-ref 'recipient params)
+                 From:    (or (alist-ref 'from params) (notification-mail-from))
+                 Subject: (sprintf (or (alist-ref 'subject params) "[pox] changes by ~A") user)
+                 Body:    new-tasks
+                 Attachments: (and (attach-diff-to-notification-mails?)
+                                   `((File: "changes.diff" Body: ,diff)))))))
 
 )
