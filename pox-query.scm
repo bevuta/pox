56380d4ccd96b2ed4950c096311f83774ce25b71
Moritz Heidkamp <moritz@twoticketsplease.de>
2011-09-30 13:19:58 +0200

    pox-query: rewrite all converters in terms of the new `define-converter' syntax and add `as-ignore'

--- pox-query.scm
+++ pox-query.scm
@@ -1,48 +1,47 @@
 (module pox-query
 
-(as-grouping as-filter)
+(as-grouping as-filter as-ignore)
 
 (import chicken scheme)
 (use extras data-structures ports srfi-1 srfi-13 sexpressive)
 
-(define filter-syntax
+(define-syntax define-converter
+  (syntax-rules ()
+    ((_ name syntax fn default)
+     (begin
+       (define name #f)
+       (let* ((syntax* syntax)
+              (convert (lambda (str)
+                         (parameterize ((sexpressive syntax*))
+                           (call-with-input-string str
+                             (lambda (in)
+                               (port-fold fn '() (lambda () (read* in)))))))))
+         (set! name
+               (lambda (variable params)
+                 (let ((val (alist-ref variable params)))
+                   (if val (convert val) default)))))))
+    ((_ name syntax fn)
+     (define-converter name syntax fn '()))
+    ((_ name syntax)
+     (define-converter name syntax cons))))
+
+(define-converter as-filter
   (append (syntax:whitespace)
-          (syntax:symbols as-string: #t)))
+          (syntax:symbols as-string: #t))
+  (lambda (x filter)
+    (if (and (string? x) (< (string-length x) 2))
+        filter
+        (cons (if (eq? #\: (string-ref x 0))
+                  (string->keyword (string-drop x 1))
+                  x)
+              filter))))
 
-(define (read-filter in)
-  (parameterize ((sexpressive filter-syntax))
-    (map (lambda (x)
-           (if (eq? #\: (string-ref x 0))
-               (string->keyword (string-drop x 1))
-               x))
-         (remove (lambda (x)
-                   (and (string? x)
-                        (< (string-length x) 2)))
-                 (read-file in read*))))) 
-
-(define (string->filter str)
-  (call-with-input-string str read-filter))
-
-(define grouping-syntax
+(define-converter as-grouping
   (append (syntax:whitespace)
           (syntax:symbols)))
 
-(define (read-grouping in)
-  (parameterize ((sexpressive grouping-syntax))
-    (read-file in read*)))
-
-(define (string->grouping str)
-  (call-with-input-string str read-grouping))
-
-(define (make-request-var-converter convert #!optional default)
-  (lambda (variable params)
-    (let ((val (alist-ref variable params)))
-      (if val (convert val) default))))
-
-(define as-grouping
-  (make-request-var-converter string->grouping '()))
-
-(define as-filter
-  (make-request-var-converter string->filter '()))
+(define-converter as-ignore
+  (append (syntax:whitespace)
+          (syntax:symbols)))
 
 )
